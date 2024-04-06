b8351c64de33cc467cefc0e60dfca2ba95a333d0
noorjahan123 <noorjahancs81@gmail.com>
2024-04-06 07:49:04 +0530

    Hello

--- TunehubWebproject/src/main/java/com/example/tunehub/controller/UserController.java
+++ TunehubWebproject/src/main/java/com/example/tunehub/controller/UserController.java
@@ -7,12 +7,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.example.tunehub.entites.Song;
 import com.example.tunehub.entites.User;
